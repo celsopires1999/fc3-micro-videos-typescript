e8e1bb4f14c117c4ce1d0e81a5b38d37d3857a80
celsopires1999 <celso.pires1999@gmail.com>
2023-10-08 01:07:22 +0000

    config and database nest modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CategoriesModule } from "./nest-modules/categories-module/categories.module";
+import { DatabaseModule } from "./nest-modules/database-module/database.module";
+import { ConfigModule } from "./nest-modules/config-module/config.module";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })

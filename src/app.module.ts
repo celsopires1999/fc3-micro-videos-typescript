246a08bed38a855567737f2ee8d55affcdc91e70
celsopires1999 <celso.pires1999@gmail.com>
2023-10-09 01:53:25 +0000

    unit tests on categories controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { CategoriesModule } from "./nest-modules/categories-module/categories.module";
 import { DatabaseModule } from "./nest-modules/database-module/database.module";
 import { ConfigModule } from "./nest-modules/config-module/config.module";
+import { SharedModule } from "./nest-modules/shared-module/shared.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    CategoriesModule,
+    SharedModule,
+  ],
 })
 export class AppModule {}

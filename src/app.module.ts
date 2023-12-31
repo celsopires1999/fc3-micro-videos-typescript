694edab78fe99532b8eb0fae2a233f0cd5df254a
celsopires1999 <celso.pires1999@gmail.com>
2023-12-31 03:14:27 +0000

    Organizando ApplicationService e DomainEventMediator

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { CastMembersModule } from "./nest-modules/cast-members-module/cast-membe
 import { CategoriesModule } from "./nest-modules/categories-module/categories.module";
 import { ConfigModule } from "./nest-modules/config-module/config.module";
 import { DatabaseModule } from "./nest-modules/database-module/database.module";
+import { EventModule } from "./nest-modules/event-module/event.module";
 import { GenresModule } from "./nest-modules/genres-module/genres.module";
 import { SharedModule } from "./nest-modules/shared-module/shared.module";
+import { UseCaseModule } from "./nest-modules/use-case-module/use-case.module";
 import { VideosModule } from "./nest-modules/videos-module/videos.module";
-import { EventModule } from "./nest-modules/event-module/event.module";
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { EventModule } from "./nest-modules/event-module/event.module";
     DatabaseModule,
     SharedModule,
     EventModule,
+    UseCaseModule,
     CategoriesModule,
     CastMembersModule,
     GenresModule,
--- src/nest-modules/event-module/event.module.ts
+++ src/nest-modules/event-module/event.module.ts
@@ -1,11 +1,23 @@
-import { Module } from "@nestjs/common";
-import { EventEmitterModule } from "@nestjs/event-emitter";
-import { FakeService } from "./fake.service";
+import { DomainEventMediator } from "@core/shared/domain/events/domain-event-mediator";
+import { Global, Module } from "@nestjs/common";
+import { EventEmitter2, EventEmitterModule } from "@nestjs/event-emitter";
 import { FakeController } from "./fake.controller";
+import { FakeService } from "./fake.service";
 
+@Global()
 @Module({
   imports: [EventEmitterModule.forRoot()],
   controllers: [FakeController],
-  providers: [FakeService],
+  providers: [
+    FakeService,
+    {
+      provide: DomainEventMediator,
+      useFactory: (eventEmitter: EventEmitter2) => {
+        return new DomainEventMediator(eventEmitter);
+      },
+      inject: [EventEmitter2],
+    },
+  ],
+  exports: [DomainEventMediator],
 })
 export class EventModule {}

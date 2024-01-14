e5988ae7bb9cac983d78fea671a1bc3f042b464d
celsopires1999 <celso.pires1999@gmail.com>
2024-01-14 18:42:26 -0300

    Eventos de integração vs eventos de domínio

--- src/core/shared/domain/events/domain-event.interface.ts
+++ src/core/shared/domain/events/domain-event.interface.ts
@@ -3,4 +3,13 @@ export interface IDomainEvent {
   aggregate_id: Uuid;
   occurred_on: Date;
   event_version: number;
+
+  getIntegrationEvent?(): IIntegrationEvent;
+}
+
+export interface IIntegrationEvent<T = any> {
+  event_version: number;
+  occurred_on: Date;
+  payload: T;
+  event_name: string;
 }

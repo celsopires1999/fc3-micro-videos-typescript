cf8cedae6aaab558bf22bf55bdf31822600e0b64
celsopires1999 <celso.pires1999@gmail.com>
2024-01-16 17:47:54 -0300

    Criando consumidor do vídeo

--- src/nest-modules/videos-module/videos.module.ts
+++ src/nest-modules/videos-module/videos.module.ts
@@ -14,6 +14,7 @@ import { GenresModule } from "../genres-module/genres.module";
 import { RabbitmqModule } from "../rabbitmq-module/rabbitmq.module";
 import { VideosController } from "./videos.controller";
 import { VIDEOS_PROVIDERS } from "./videos.providers";
+import { VideosConsumers } from "./videos.consumers";
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { VIDEOS_PROVIDERS } from "./videos.providers";
     ...Object.values(VIDEOS_PROVIDERS.REPOSITORIES),
     ...Object.values(VIDEOS_PROVIDERS.USE_CASES),
     ...Object.values(VIDEOS_PROVIDERS.HANDLERS),
+    VideosConsumers,
   ],
   //exports: [VIDEOS_PROVIDERS.REPOSITORIES.VIDEO_REPOSITORY.provide],
 })
